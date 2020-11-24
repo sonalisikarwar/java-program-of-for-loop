ad3254653bf71c0d82f442196abc18a0bf1bb01c
SAPNA <47707734+sonalisikarwar@users.noreply.github.com>
2020-11-24 20:57:16 +0530

    Add files via upload

---
+++ AreaCircle.java
@@ -0,0 +1,11 @@
+public class AreaCircle{	
+public static void main(String[] args) {		
+ double pi,r,a;
+r=10.8;     //radius of circle
+pi=3.1416; //	pi , 	approximity
+a=pi*r*r;  //compute area
+
+
+System.out.println("Area of circle is a  " + a );	
+}
+}
