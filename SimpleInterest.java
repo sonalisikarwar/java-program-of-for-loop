5054cc648f97dfded52bd7ba379732c5901efb14
SAPNA <47707734+sonalisikarwar@users.noreply.github.com>
2020-11-26 00:41:04 +0530

    Add files via upload
    file added

---
+++ SimpleInterest.java
@@ -0,0 +1,12 @@
+public class SimpleInterest{	
+public static void main(String[] args) {		
+ double si,p,r,t;
+p = 1000;       //principal; 
+r = 4;         //rate of interest
+t = 2 ;        //time in years
+si=p*r*t/100;
+
+
+System.out.println("simple interest is   " + si);	
+}
+}
