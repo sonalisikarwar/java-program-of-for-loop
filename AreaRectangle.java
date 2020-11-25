79e8cef950af0f12ca7aba8066b61d6360c88b04
SAPNA <47707734+sonalisikarwar@users.noreply.github.com>
2020-11-26 00:49:18 +0530

    Add files via upload
    file added

---
+++ AreaRectangle.java
@@ -0,0 +1,11 @@
+public class AreaRectangle{	
+public static void main(String[] args) {		
+ int a,l,b;
+l=10;      //length of rectangle; 
+b=4;      //breadth of rectangle
+a=l*b;
+
+
+System.out.println("Area of a rectangle is   " + a);	
+}
+}
