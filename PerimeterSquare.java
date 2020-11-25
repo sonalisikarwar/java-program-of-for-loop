434ee350650825b3daa349676cf116e0cae83f8d
SAPNA <47707734+sonalisikarwar@users.noreply.github.com>
2020-11-26 00:39:59 +0530

    Add files via upload
    file added

---
+++ PerimeterSquare.java
@@ -0,0 +1,10 @@
+public class PerimeterSquare{	
+public static void main(String[] args) {		
+ double s,p;
+s=10;     //side of square; 
+p=s*4;  //compute peimeter
+
+
+System.out.println("Perimeter of circle is a  " + p);	
+}
+}
