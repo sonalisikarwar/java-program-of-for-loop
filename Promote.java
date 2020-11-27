a3eb78a4bb09a72d5b846307680114fdcb5df396
SAPNA <47707734+sonalisikarwar@users.noreply.github.com>
2020-11-27 19:36:31 +0530

    Add files via upload
    file added

---
+++ Promote.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author SAPNA
+ */
+public class Promote {
+    public static void main(String args[]){
+        byte b =42;
+        char c='a';
+        short s=1024;
+        int i=50000;
+        float f=5.67f;
+        double d=.1234;
+        double result=(f*b) + (i / c) - (d *s);
+        System.out.println("result =" + result);
+        
+    }
+    
+}
