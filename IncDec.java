abb9aebb01440a2b8ac22d23423ed3486b6eb25e
SAPNA <47707734+sonalisikarwar@users.noreply.github.com>
2020-11-30 18:43:34 +0530

    Add files via upload
    updated

---
+++ IncDec.java
@@ -0,0 +1,18 @@
+
+package shivaafoundation.javatutorial;
+public class IncDec {
+    public static void main(String args[]){
+        int a=1;
+        int b=2;
+        int c=3;
+        int d=4;
+        c = ++b;
+        d = a++;
+        c++;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d =  " + d);
+    }
+    
+}
