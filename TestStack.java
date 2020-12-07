d8f6d3018421801db109202bd8f26dd952625249
SAPNA <47707734+sonalisikarwar@users.noreply.github.com>
2020-12-07 18:20:56 +0530

    Add files via upload
    updated

---
+++ TestStack.java
@@ -0,0 +1,25 @@
+
+package shivaafoundation.javatutorial;
+
+
+public class TestStack {
+    public static void main(String args[]){
+    Stack mystack1= new Stack();
+    Stack mystack2 = new Stack();
+    
+    //push some numbers on to the stack
+    for(int i=0; i<10; i++) mystack1.push(i);
+    for(int i=10; i<20; i++) mustack2.push(i);
+    //pop those numbers off the stack
+    System.ot.println("Stack in mystack1:");
+    for(int i=0;i<10;i++)
+    System.out.println(mystack1.pop());
+    
+    System.out.println("Stack in mystack2:");
+    for(int i=0; i<10; i++)
+    System.out.println(mystack2.pop());
+        
+        
+    }
+    
+}
