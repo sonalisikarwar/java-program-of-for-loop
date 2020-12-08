e3009ec8be26dbb1cfc621ff248d4603d441315c
SAPNA <47707734+sonalisikarwar@users.noreply.github.com>
2020-12-08 22:30:18 +0530

    Add files via upload
    updated

---
+++ DateApp.java
@@ -0,0 +1,13 @@
+
+package shivaafoundation.javatutorial;
+import java.util.Date;
+public class DateApp {
+    public static void main(String args[])
+    {
+        Date today = new Date();
+        System.out.println(today);
+        
+    }
+    
+    
+}
